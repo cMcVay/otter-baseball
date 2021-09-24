51557afe18f0cda56248cb9db142c73239477ad2
cMcVay <86023551+cMcVay@users.noreply.github.com>
2021-09-24 15:31:18 -0400

    fix create standings

--- src/CreateStandings.js
+++ src/CreateStandings.js
@@ -1,5 +1,5 @@
 import {teams} from "./Teams";
 
-const CreateStandings = [...teams].sort((a,b) => (a.record.wins < b.record.wins) ? 1 : (a.record.wins === b.record.wins) ? ((a.record.tiebreak[b] < b.record.tiebreak[a]) ? 1 : (a.record.tiebreak[b] === b.record.tiebreak[a]) ? ((a.stats.totalOP < b.stats.totalOP) ? 1 : -1): -1): -1);
+const CreateStandings = [...teams].sort((a,b) => (a.record.wins < b.record.wins) ? 1 : (a.record.wins === b.record.wins) ? ((a.record.tiebreak[b] > b.record.tiebreak[a]) ? 1 : (a.record.tiebreak[b] === b.record.tiebreak[a]) ? ((a.stats.totalOP < b.stats.totalOP) ? 1 : -1): -1): -1);
 
 export default CreateStandings;
