29daca93c1bfee25b9ba3feb7202dc737ab97430
cMcVay <86023551+cMcVay@users.noreply.github.com>
2021-09-24 16:28:19 -0400

    standings text 7

--- src/CreateStandings.js
+++ src/CreateStandings.js
@@ -1,13 +1,16 @@
 import {teams} from "./Teams";
 
-
+const getNum = (x, place) => {
+    console.log(x.profile.name + '' + x.record.tiebreak[place]);
+    return x.record.tiebreak[place];
+};
 
 //const CreateStandings = [...teams].sort((a,b) => (a.record.wins < b.record.wins) ? 1 : (a.record.wins === b.record.wins) ? ((a.record.tiebreak[b] > b.record.tiebreak[a]) ? 1 : (a.record.tiebreak[b] === b.record.tiebreak[a]) ? ((a.stats.totalOP < b.stats.totalOP) ? 1 : -1): -1): -1);
 const CreateStandings = [...teams].sort((a,b) =>
     (a.record.wins > b.record.wins) ? -1 : (
         (a.record.wins === b.record.wins) ? (
-            (a.record.tiebreak[b] > b.record.tiebreak[a]) ? -1 : (
-                (a.record.tiebreak[b] === b.record.tiebreak[a] ? ((a.stats.totalOP > b.stats.totalOP) ? -1 : 1)
+            (getNum(a, b) > getNum(b, a)) ? -1 : (
+                ((getNum(a, b) === getNum(b, a)) ? ((a.stats.totalOP > b.stats.totalOP) ? -1 : 1)
                 : 1)
             )
         ) : 1
