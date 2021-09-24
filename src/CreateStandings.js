284f10d515b9f01ed3a401362deb4bca31f26f5b
cMcVay <86023551+cMcVay@users.noreply.github.com>
2021-09-24 16:18:39 -0400

    standing test 5

--- src/CreateStandings.js
+++ src/CreateStandings.js
@@ -7,8 +7,10 @@ const CreateStandings = [...teams].sort((a,b) =>
     (a.record.wins > b.record.wins) ? -1 : (
         (a.record.wins === b.record.wins) ? (
             (a.record.tiebreak[b] > b.record.tiebreak[a]) ? -1 : (
-                (a.stats.totalOP > b.stats.totalOP) ? -1 : 1)
-                )
-            : 1)
-        );
+                (a.record.tiebreak[b] === b.record.tiebreak[a] ? ((a.stats.totalOP > b.stats.totalOP) ? -1 : 1)
+                : 1)
+            )
+        ) : 1
+    )
+);
 export default CreateStandings;
